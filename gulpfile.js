533d103a1acc9ef707e007f3b1f70a46a58c23ec
Rulox <marreror91@gmail.com>
2018-05-01 21:29:04 +0100

    Change webpack for Gulp to make it simpler

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+// Static Server + watching scss/html files
+gulp.task('serve', function() {
+
+    browserSync.init({
+        server: "./public"
+    });
+
+    gulp.watch("source/style/**/*.scss", ['sass']);
+    gulp.watch("public/*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("source/style/style.scss")
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest("public/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('dev', ['sass', 'serve']);
